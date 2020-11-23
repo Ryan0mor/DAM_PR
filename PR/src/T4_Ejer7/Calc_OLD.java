319186c7e29455c48ccccb2db269661be0c4fe25
Ryan0mor <71499530+Ryan0mor@users.noreply.github.com>
2020-11-23 20:22:52 +0100

    ktc

---
+++ PR/src/T4_Ejer7/Calc_OLD.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package T4_Ejer7;
+
+/**
+ *
+ * @author Ryan
+ */
+public class Calc_OLD {
+
+    
+    public static double suma(double numero1, double numero2){   
+        return numero1 + numero2;
+    }
+    public static double resta(double numero1, double numero2){
+        return numero1 - numero2;
+    }   
+    public static double multiplica(double numero1, double numero2){   
+        return numero1 * numero2;
+    }
+    public static double divide(double numero1, double numero2){  
+        return numero1 / numero2;
+    } 
+
+
+}
--- PR/src/T4_Ejer7/Main.java
+++ PR/src/T4_Ejer7/Main.java
@@ -10,5 +10,38 @@ package T4_Ejer7;
  * @author Ryan
  */
 public class Main {
+    public static void main(String[] args) {
+        double num1;
+        double den1;
+        double num2;
+        double den2;
+        String operacion;
+        
+        do {            
+            num1 = Consola.pideDouble("Dame el nuemerador 1");
+            den1 = Consola.pideDouble("Dame el denominador 1");
+            num2 = Consola.pideDouble("Dame el nuemerador 2");
+            den2 = Consola.pideDouble("Dame el denominador 2");
+            operacion = Consola.pideCadena("Selecciona la operación a realizar: \"+”, \"-\", \"*” ó \"/”");
+            switch (operacion){
+                case "+":
+                    System.out.println("La suma es: ");
+                    break;
+                case "- ":
+                    System.out.println("La suma es: ");
+                    break;
+                case "*":
+                    System.out.println("La suma es: ");
+                    break;
+                case "/":
+                    System.out.println("La suma es: ");
+                    break;
+                default:
+                    System.out.println("operación incorrecta");
+                    
+            }
+            
+        } while (num1 != 0);
+    }
     
 }
